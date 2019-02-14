fdeef3c83cb3e41543bdc9a6b0bd8183ea662994
Arash Paknezad <a.paknezad@hotmail.com>
2019-02-14 21:31:27 +0100

    refactored code

--- week1/public/js/modules/api.js
+++ week1/public/js/modules/api.js
@@ -1,5 +1,7 @@
 const BASE_API_URL = 'https://restcountries.eu/rest/v2';
 
+/* TODO generate the API urls */
+
 const api = {
     async getAll() {
         const response = await fetch(`${BASE_API_URL}/all?fields=name;alpha2Code;flag`);
@@ -7,7 +9,7 @@ const api = {
     },
 
     async get(code) {
-        const response = await fetch(`${BASE_API_URL}/alpha/${code}?fields=name;flag`);
+        const response = await fetch(`${BASE_API_URL}/alpha/${code}?fields=name;flag;capital;region;subregion;latlng;timezones;currencies;languages`);
         return response.json();
     }
 };
