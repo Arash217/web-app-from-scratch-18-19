a26a6e30e9d69fd8344b03fe78bffe69dee65a78
Arash Paknezad <a.paknezad@hotmail.com>
2019-02-15 09:46:30 +0100

    fetch more data from API

--- week2/public/js/modules/api.js
+++ week2/public/js/modules/api.js
@@ -4,7 +4,7 @@ const BASE_API_URL = 'https://restcountries.eu/rest/v2';
 
 const api = {
     async getAll() {
-        const response = await fetch(`${BASE_API_URL}/all?fields=name;alpha2Code;flag`);
+        const response = await fetch(`${BASE_API_URL}/all?fields=name;alpha2Code;flag;capital;region;subregion;latlng;timezones;currencies;languages`);
         return response.json();
     },
 
