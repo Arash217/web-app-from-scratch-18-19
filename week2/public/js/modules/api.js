4d31a4d2d1208f480dcef747eb0bf0f757bc7073
Arash Paknezad <a.paknezad@hotmail.com>
2019-02-22 23:38:22 +0100

    changed naming

--- week2/public/js/modules/api.js
+++ week2/public/js/modules/api.js
@@ -1,22 +1,20 @@
 import utils from './utils.js';
 
-const getAllRequiredFields = ['name', 'alpha2Code', 'flag'];
-const getRequiredFields = [
-    ...getAllRequiredFields, 'capital', 'region', 'subregion', 'latlng', 'timezones', 'currencies', 'languages'
-];
-const getAllURLParameters = utils.URLParameterBuilder(getAllRequiredFields);
-const getURLParameters = utils.URLParameterBuilder(getRequiredFields);
+const getAllFields = ['name', 'alpha2Code', 'flag'];
+const getFields = [...getAllFields, 'capital', 'region', 'subregion', 'latlng', 'timezones', 'currencies', 'languages'];
+const getAllURLParameters = utils.URLParameterBuilder(getAllFields);
+const getURLParameters = utils.URLParameterBuilder(getFields);
 const BASE_API_URL = 'https://restcountries.eu/rest/v2';
 
 const api = {
     getAll: utils.errorMiddleware(async () => {
         const data = await utils.fetchRequest(`${BASE_API_URL}/all?fields=${getAllURLParameters}`);
-        return utils.extractFromArray(data, getAllRequiredFields);
+        return utils.extractFromArray(data, getAllFields);
     }),
 
     get: utils.errorMiddleware(async code => {
         const data = await utils.fetchRequest(`${BASE_API_URL}/alpha/${code}?fields=${getURLParameters}`);
-        return utils.extractFromObject(data, getRequiredFields);
+        return utils.extractFromObject(data, getFields);
     })
 };
 
