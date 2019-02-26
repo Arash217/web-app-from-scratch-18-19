46b5fe10f25371a63d1baae2fd784bc5837a7166
Arash Paknezad <a.paknezad@hotmail.com>
2019-02-26 10:22:29 +0100

    changed variable  name

--- week3/public/js/modules/api.js
+++ week3/public/js/modules/api.js
@@ -4,14 +4,14 @@ const getAllFields = ['name', 'alpha2Code', 'flag'];
 const getFields = [...getAllFields, 'capital', 'region', 'subregion', 'latlng', 'timezones', 'currencies', 'languages'];
 const getAllURLParameters = URLParameterBuilder(getAllFields);
 const getURLParameters = URLParameterBuilder(getFields);
-const BASE_API_URL = 'https://restcountries.eu/rest/v2';
+const baseApiURL = 'https://restcountries.eu/rest/v2';
 
 export const getAll = errorMiddleware(async () => {
-    const data = await request(`${BASE_API_URL}/all?fields=${getAllURLParameters}`);
+    const data = await request(`${baseApiURL}/all?fields=${getAllURLParameters}`);
     return extractFromArray(data, getAllFields);
 });
 
 export const get = errorMiddleware(async code => {
-    const data = await request(`${BASE_API_URL}/alpha/${code}?fields=${getURLParameters}`);
+    const data = await request(`${baseApiURL}/alpha/${code}?fields=${getURLParameters}`);
     return extractFromObject(data, getFields);
 });
